77efcc4bbef3d9e00f34f2089289d0a8c8f4db79
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2023-09-28 18:09:02 +0900

    왼쪽 이동 상태 완료 및 오류 수정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -13,8 +13,8 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir_LR += 1
-            elif event.type == SDLK_LEFT:
-                dir_LR -= -1
+            elif event.key == SDLK_LEFT:
+                dir_LR -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -32,8 +32,10 @@ while running:
     clear_canvas()
     if dir_LR == 0: # IDLE 상태
         character.clip_draw(0, 728, 96, 104, window_x, 50)
-    elif dir_LR == +1:  # 오른쪽 이동 상태
+    elif dir_LR == 1:  # 오른쪽 이동 상태
         character.clip_draw(frame * 96, 0, 96, 104, window_x, 50)
+    elif dir_LR == -1:  # 왼쪽 이동 상태
+        character.clip_draw(frame * 97, 207, 96, 104, window_x, 50)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 10

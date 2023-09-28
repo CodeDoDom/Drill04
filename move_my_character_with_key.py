13c3d0454ae3eccf1ac8c4f19f12d26cad68fa29
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2023-09-28 17:52:40 +0900

    오른쪽 이동 완료

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -32,7 +32,8 @@ while running:
     clear_canvas()
     if dir_LR == 0: # IDLE 상태
         character.clip_draw(0, 728, 96, 104, window_x, 50)
-
+    elif dir_LR == +1:  # 오른쪽 이동 상태
+        character.clip_draw(frame * 96, 0, 96, 104, window_x, 50)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 10

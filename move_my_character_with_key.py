98573f1d0f1cc956c464ff05c648558df9244148
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2023-09-28 18:33:03 +0900

    위쪽 이동 완료

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,7 +4,7 @@ open_canvas()
 character = load_image('animation_sheet_link.png')
 
 def handle_events():
-    global running, dir_LR
+    global running, dir_LR, dir_UD
 
     events = get_events()
     for event in events:
@@ -15,6 +15,10 @@ def handle_events():
                 dir_LR += 1
             elif event.key == SDLK_LEFT:
                 dir_LR -= 1
+            elif event.key == SDLK_UP:
+                dir_UD += 1
+            elif event.key == SDLK_DOWN:
+                dir_UD -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -22,24 +26,34 @@ def handle_events():
                 dir_LR -= 1
             elif event.key == SDLK_LEFT:
                 dir_LR += 1
+            elif event.key == SDLK_UP:
+                dir_UD -= 1
+            elif event.key == SDLK_DOWN:
+                dir_UD += 1
+
 
 running = True
 window_x = 800 // 2
+window_y = 50
 frame = 0
 dir_LR = 0
+dir_UD = 0
 
 while running:
     clear_canvas()
-    if dir_LR == 0: # IDLE 상태
-        character.clip_draw(0, 728, 96, 104, window_x, 50)
+    if dir_LR == 0 and dir_UD == 0: # IDLE 상태
+        character.clip_draw(0, 728, 96, 104, window_x, window_y)
     elif dir_LR == 1:  # 오른쪽 이동 상태
-        character.clip_draw(frame * 96, 0, 96, 104, window_x, 50)
+        character.clip_draw(frame * 96, 0, 96, 104, window_x, window_y)
     elif dir_LR == -1:  # 왼쪽 이동 상태
-        character.clip_draw(frame * 97, 207, 96, 104, window_x, 50)
+        character.clip_draw(frame * 97, 208, 96, 104, window_x, window_y)
+    elif dir_UD == 1:   # 위쪽 이동 상태
+        character.clip_draw(frame * 96, 104, 96, 104, window_x, window_y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 10
     window_x += dir_LR * 5
+    window_y += dir_UD * 5
     delay(0.05)
 
 close_canvas()

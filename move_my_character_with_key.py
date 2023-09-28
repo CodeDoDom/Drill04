2a8a3e0a45988679b0eb75d85760a185b3094d7b
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2023-09-28 17:51:55 +0900

    IDLE 상태 완료

---
+++ move_my_character_with_key.py
@@ -0,0 +1,42 @@
+from pico2d import *
+
+open_canvas()
+character = load_image('animation_sheet_link.png')
+
+def handle_events():
+    global running, dir_LR
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_LR += 1
+            elif event.type == SDLK_LEFT:
+                dir_LR -= -1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_LR -= 1
+            elif event.key == SDLK_LEFT:
+                dir_LR += 1
+
+running = True
+window_x = 800 // 2
+frame = 0
+dir_LR = 0
+
+while running:
+    clear_canvas()
+    if dir_LR == 0: # IDLE 상태
+        character.clip_draw(0, 728, 96, 104, window_x, 50)
+
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 10
+    window_x += dir_LR * 5
+    delay(0.05)
+
+close_canvas()
